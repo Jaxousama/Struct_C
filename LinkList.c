cf9b9b9981bfdc155eacb305f8f16dda74e52a58
Joris <jojo38260@hotmail.fr>
2024-10-06 23:55:44 +0200

    Fin des Linklist pour l'instant

--- LinkList.c
+++ LinkList.c
@@ -7,14 +7,21 @@ typedef struct Node{
     struct Node* next;
 }Cell;
 
+int est_vide_LinkList(Cell* c){
+    if(c == NULL || c ==0){
+        return 1;
+    }
+    return 0;
+}
 
 int affiche(Cell* c){
-    if(c==NULL){
-        printf("Erreur liste vide");
+    if(est_vide_LinkList(c)){
+        printf("Vide\n");
         return -1;
     }
     if(c->next == NULL){
         printf("%d\n",c->donnee);
+        return 0;
     }
     printf("%d",c->donnee);
     c=c->next;
@@ -32,23 +39,23 @@ Cell* init(int x){
     return c;
 }
 
-int ajout_Cell_Queue(Cell* c,int x){
-    if(c==NULL){
-        printf("Erreur liste vide");
-        return -1;
+int ajout_Cell_Queue(Cell** c,int x){
+    if(est_vide_LinkList((*c))){
+        (*c) = init(x);
+        return 0;
     }
     Cell* c_2 = init(x);
-    while (c->next != NULL){
-        c = c->next;
+    while ((*c)->next != NULL){
+        (*c) = (*c)->next;
     }
-    c->next = c_2;
+    (*c)->next = c_2;
     return 0;
 }
 
 int ajout_Cell_Tete(Cell** c,int x){
-    if(c==NULL){
-        printf("Erreur liste vide");
-        return -1;
+    if(est_vide_LinkList((*c))){
+        (*c) = init(x);
+        return 0;
     }
     Cell* c_2 ;
     c_2 = init(x);
@@ -57,13 +64,13 @@ int ajout_Cell_Tete(Cell** c,int x){
 }
 
 int supprimer_Queue(Cell* c){
-    if(c==NULL){
+    if(est_vide_LinkList(c)){
         printf("Erreur liste vide");
         return -1;
     }
     if(c->next == NULL){
-        printf("Erreur pas de suivant");
-        return -1;
+        free(c);
+        return 0;
     }
 
     Cell* Actuel =c;
@@ -73,46 +80,109 @@ int supprimer_Queue(Cell* c){
         Actuel = Suivant;
         Suivant = Suivant->next;
     }
-
     Actuel->next = NULL;
+    free(Suivant);
     return 0;
 }
 
 int supprimer_tete(Cell** c){
-    if((*c)==NULL){
+    if(est_vide_LinkList((*c))){
         printf("Erreur liste vide");
-        return -1;
-    }
-    if((*c)->next == NULL){
-        printf("Erreur pas de suivant");
         return -1;
     }
     *c=(*c)->next;
 }
 
 int recherche_LinkList(Cell* c,int x){
-    if(c == NULL){
+    if(est_vide_LinkList(c)){
         printf("Erreur liste vide");
         return -1;
     }
+
     Cell* Actuel = c;
+
     while(Actuel->donnee == x && Actuel != NULL){
         Actuel = Actuel -> next;
     }
     if(Actuel == NULL){
         printf("Pas trouvé");
+        return 0;
     }
+
     printf("Trouvé");
     return 1;
 }
 
+
+int supprimer_first_Occurence(Cell** c, int x){
+    if(est_vide_LinkList((*c))){
+        printf("Erreur liste vide\n");
+        return -1;
+    }
+
+    Cell* Actuel = (*c);
+    Cell* Suivant = (*c)->next;
+
+    if(Actuel->donnee == x){
+        if(supprimer_tete(c) == -1){return -1;};
+        return 1;
+    }
+
+    while (Suivant != NULL){
+        if(Suivant->donnee == x){
+            Actuel->next = Suivant->next;
+            free(Suivant);
+            return 0;
+        }
+        Suivant = Suivant->next;
+        Actuel = Actuel->next;
+    }
+
+    printf("Pas trouvé\n");
+    return -1;
+
+}
+
+int supprimer_all_Occurence(Cell** c,int x){
+    if(est_vide_LinkList((*c))){
+        printf("Erreur liste vide\n");
+        return -1;
+    }
+    
+
+    while((*c)!= NULL && (*c)->donnee == x){
+        *c=(*c)->next;
+    }
+
+    if((*c) == NULL){
+        return 0;
+    }
+
+    Cell* Actuel = (*c);
+    Cell* Suivant = (*c)->next;
+    
+    while (Suivant != NULL){
+        if((Suivant->donnee == x)){
+            Actuel->next = Suivant->next;
+            free(Suivant);
+            Suivant = Actuel->next;
+        }
+        else{
+            Suivant = Suivant->next;
+            Actuel = Actuel->next;
+        }
+    }
+    
+
+    return 0;
+}
+
 int main(){
-    Cell* c =init(3);
-    affiche(c);
+    Cell* c =NULL ;
+
     ajout_Cell_Tete(&c,45);
     affiche(c);
-    supprimer_tete(&c);
-    ajout_Cell_Tete(&c,45);
-    ajout_Cell_Tete(&c,55);
+    supprimer_first_Occurence(&c,45);
     affiche(c);
+    
 }
